68376640e5c4f367d90018902ecba7261280d4b1
aydildv <ad1793@georgetown.edu>
2025-04-13 17:36:49 -0400

    Added docs folder for site output

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/Rally-House/",
+  build: {
+    outDir: "docs",
+  },
   plugins: [vue()],
 });
